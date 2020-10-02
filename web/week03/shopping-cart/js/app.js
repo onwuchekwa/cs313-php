8aa590a731d8d31dcbc2974c28ae933c22703eb1
onwuchekwa <donsonde@gmail.com>
2020-10-02 09:05:24 +0000

    g

--- web/week03/shopping-cart/js/app.js
+++ web/week03/shopping-cart/js/app.js
@@ -1,8 +1,8 @@
- jQuery(document).ready(function() {
-    jQuery(".openModal").click(function(e) {
+  $(document).ready(function() {
+    $(".openModal").click(function(e) {
       let itemId = $(this).data("itemid");
       e.preventDefault();
-      jQuery.ajax({
+        $.ajax({
             url: 'browse_item.php', 
             type: 'POST', 
             data: itemId,
