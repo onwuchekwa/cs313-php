527bb3ab50db17c14a8c1ab7f6d7ce9a60916a9e
onwuchekwa <donsonde@gmail.com>
2020-10-03 12:27:41 +0000

    b

--- web/week03/shopping-cart/browse_item.php
+++ web/week03/shopping-cart/browse_item.php
@@ -7,7 +7,7 @@
 <main>
     <section>
         <h1 class="heading">Product Listing</h1>
-        <div class="view-cart-nav float-right">
+        <div class="view-cart-nav">
           <a href="view_cart.php" title="View Cart" class="btn btn-primary float-right"><img src="images/add-to-cart.png" alt="vew cart"> View Cart</a>
         </div>        
         <div class="product-container">
