4499538a743ee09bd9402c3808055d59a332b808
onwuchekwa <donsonde@gmail.com>
2020-10-03 18:55:42 +0000

    add responsiveness to tables

--- web/week03/shopping-cart/confirmation.php
+++ web/week03/shopping-cart/confirmation.php
@@ -22,7 +22,7 @@
     <div>
         <div class="heading">Confirm Purchase</div>
         <div id="cartItems" class="checkout-container">
-            <table class="customerInfo table table-striped table-bordered">
+            <table class="customerInfo table table-striped table-bordered table-responsive">
                 <tbody>
                     <tr>
                         <td class="text-left"><strong>Client Name:</strong></td>
@@ -42,7 +42,7 @@
                     </tr>
                 </tbody>                        
             </table>
-            <table class="cartTable table table-striped table-bordered">
+            <table class="cartTable table table-striped table-bordered table-responsive">
                 <tbody>
                     <tr>
                         <th><strong>Product Name</strong></th>
