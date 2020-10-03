1e2fccba9c59172f4f9c5c957dd1ed561288ce92
onwuchekwa <donsonde@gmail.com>
2020-10-03 18:19:16 +0000

    fixed width issue

--- web/week03/shopping-cart/thanks.php
+++ web/week03/shopping-cart/thanks.php
@@ -4,9 +4,9 @@
 ?>
 
 <main>
-    <div>
+    <div class="text-center">
         <div class="heading">Appreciation</div>  
-        <p>It is a pleasure doing business with you. Your items will be shipped to you in three (3) working days<br><a href="browse_item.php" title="Return to home page">Return to Homepage</a></p>      
+        <p class="text-center">It is a pleasure doing business with you. Your items will be shipped to you in three (3) working days<br><a href="browse_item.php" title="Return to home page">Return to Homepage</a></p>      
 </div>
 </main>
 <?php include './common/footer.php'; ?>
