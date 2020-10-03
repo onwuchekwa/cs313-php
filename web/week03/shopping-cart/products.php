6ab6d469851002605b5a925808d716ca42cedea6
onwuchekwa <donsonde@gmail.com>
2020-10-03 11:55:51 +0000

    c

--- web/week03/shopping-cart/products.php
+++ web/week03/shopping-cart/products.php
@@ -4,42 +4,42 @@ class Products {
     public $productsArray = array(
         "Trouser01" => array(
             "itemId" => "1",
-            "itemCode" => "1208",
+            "itemCode" => "Trouser01",
             "itemName" => "Trouser",
             "itemPrice" => 35.25,
             "productImage" => "images/trouser.jpg"
         ), 
         "Shirt02" => array(
             "itemId" => "2",
-            "itemCode" => "1209",
+            "itemCode" => "Shirt02",
             "itemName" => "Shirt",
             "itemPrice" => 25.00,
             "productImage" => "images/shirt.jpg"
         ),
         "TShirt03" => array(
             "itemId" => "3",
-            "itemCode" => "1210",
+            "itemCode" => "TShirt03",
             "itemName" => "T-Shirt",
             "itemPrice" => 10.35,
             "productImage" => "images/tshirt.jpg"
         ),
         "Jean04" => array(
             "itemId" => "4",
-            "itemCode" => "1211",
+            "itemCode" => "Jean04",
             "itemName" => "Jean",
             "itemPrice" => 25.99,
             "productImage" => "images/jean.jpg"
         ),
         "Skirt05" => array(
             "itemId" => "5",
-            "itemCode" => "1212",
+            "itemCode" => "Skirt05",
             "itemName" => "Skirt",
             "itemPrice" => 12.36,
             "productImage" => "images/skirt.jpg"
         ),
         "Blouse06" => array(
             "itemId" => "6",
-            "itemCode" => "1213",
+            "itemCode" => "Blouse06",
             "itemName" => "Blouse",
             "itemPrice" => 8.99,
             "productImage" => "images/blouse.jpg"
