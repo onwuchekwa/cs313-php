93a8be62c578ce60a77f4dfd3d7346dab190e801
onwuchekwa <donsonde@gmail.com>
2020-10-03 11:16:56 +0000

    g

--- web/week03/shopping-cart/js/app.js
+++ web/week03/shopping-cart/js/app.js
@@ -1,33 +1,33 @@
-$(document).ready(function() {
-    function manageCart(action, itemCode) {
-        let quertString = "";
-        if(action != "") {
-            switch(action) {
-                case "add":
-                    quertString = "action=" + action + "&itemCode=" + itemCode + "&quantity=" + $("#quantity_" + itemCode).val();
-                    break;
-    
-                case "remove":
-                    quertString = "action=" + action + "&itemCode=" + itemCode;
-                    break;
-                    
-                case "empty":
-                    quertString = "action=" + action;
-                    break;
+
+function manageCart(action, itemCode) {
+    let quertString = "";
+    if(action != "") {
+        switch(action) {
+            case "add":
+                quertString = "action=" + action + "&itemCode=" + itemCode + "&quantity=" + $("#quantity_" + itemCode).val();
+                break;
+
+            case "remove":
+                quertString = "action=" + action + "&itemCode=" + itemCode;
+                break;
+                
+            case "empty":
+                quertString = "action=" + action;
+                break;
+        }
+    }
+
+    $.ajax({
+        url: "../ajax-action",
+        data: quertString,
+        type: "POST",
+        success: function(data) {
+            $("#cartItems").html(data);
+            if(action == "add") {
+                $("#add_" + itemCode + " img").attr("src", "images/icon-check.png");
+                $("#add_" + itemCode).attr("onclick", "");
             }
         }
-    
-        $.ajax({
-            url: "../ajax-action",
-            data: quertString,
-            type: "POST",
-            success: function(data) {
-                $("#cartItems").html(data);
-                if(action == "add") {
-                    $("#add_" + itemCode + " img").attr("src", "images/icon-check.png");
-                    $("#add_" + itemCode).attr("onclick", "");
-                }
-            }
-        })
-    }
-});
+    })
+}
+
