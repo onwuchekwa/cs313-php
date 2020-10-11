e67a8ed0045119da8b10b0857cd6f9aa5deedc67
onwuchekwa <donsonde@gmail.com>
2020-10-11 22:56:55 +0000

    second commit

--- web/abaonline/js/app.js
+++ web/abaonline/js/app.js
@@ -1,2 +1,13 @@
+//Date function
+function getFullDate() {
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const dt = new Date();
+    const date = dt.getDate();
+    const monthName = monthNames[dt.getMonth()];
+    const fullYear = dt.getFullYear();
+    const date_value = `${date} ${monthName}, ${fullYear}`;
+    return date_value;
+}
+
 // Footer Date
 document.getElementById("lastUpdated").innerHTML = getFullDate();
