11373c625c099c3d0612ff45e4c06711b80b4136
onwuchekwa <donsonde@gmail.com>
2020-10-12 20:46:28 +0000

    v

--- web/abaonline/view/register.php
+++ web/abaonline/view/register.php
@@ -5,37 +5,37 @@
 ?>
 
 <main>
-    <div class="col"></div>
-    <div class="col-sm-12 col-md-6">
-        <div class="card">
-            <div class="card-header">
-                Register with Us
+    <div class="row">
+        <div class="col-sm-12 col-md-6 mx-auto">
+            <div class="card">
+                <div class="card-header">
+                    Register with Us
+                </div>
+                <div class="card-body">
+                    <?php
+                        if (isset($message)) {
+                            echo $message;
+                        }
+                    ?>
+                    <form action="../actions/index.php" method="POST">
+                        <fieldset>
+                            <legend>All fields marked asterisk (*) are required</legend>
+                            <div class="form-group">
+                                <label for="firstName">First name <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control" id="firstName" placeholder="Enter first name" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="middleName">Middle name</label>
+                                <input type="text" class="form-control" id="middleName" placeholder="Enter middle name">
+                            </div>
+                        </fieldset>
+                        <input type="submit" value="Register" class="btn btn-primary">
+                        <input type="hidden" name="action" value="register">           
+                    </form>
+                </div> 
             </div>
-            <div class="card-body">
-                <?php
-                    if (isset($message)) {
-                        echo $message;
-                    }
-                ?>
-                <form action="../actions/index.php" method="POST">
-                    <fieldset>
-                        <legend>All fields marked asterisk (*) are required</legend>
-                        <div class="form-group">
-                            <label for="firstName">First name <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control" id="firstName" placeholder="Enter first name" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="middleName">Middle name</label>
-                            <input type="text" class="form-control" id="middleName" placeholder="Enter middle name">
-                        </div>
-                    </fieldset>
-                    <input type="submit" value="Register" class="btn btn-primary">
-                    <input type="hidden" name="action" value="register">           
-                </form>
-            </div> 
         </div>
     </div>
-    <div class="col"></div>
 </main>
 
 <?php include '../common/footer.php'; ?>
