78d4a34acbd8c7626ae145d37f6f44ca57384c8a
onwuchekwa <donsonde@gmail.com>
2020-10-12 00:25:41 +0000

    f

--- web/abaonline/common/footer.php
+++ web/abaonline/common/footer.php
@@ -1,4 +1,4 @@
-            <footer>
+            <footer class="navbar-light bg-light text-center">
                 <hr>
                 <p class="copy">&copy;<?php echo date('Y'); ?> Aba Online Direct, All rights Reserved.</p> 
                 <p>All images used are believed to be in <q>Fair Use</q>. Please notify the author if any are not and they will be removed.</p>
