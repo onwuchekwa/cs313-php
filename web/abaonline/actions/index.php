114e1aea862836ccc99b713eb13561cd475ae2ce
Sunday Onwuchekwa <donsonde@gmail.com>
2020-10-17 18:47:07 +0000

    n

--- web/abaonline/actions/index.php
+++ web/abaonline/actions/index.php
@@ -69,11 +69,10 @@
             // Hash the checked password
             $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-            echo 'After the files now';
-            exit;
-
             // Send the data to the model
             $regOutcome = regBusinessOwner($userName, $password, $firstName, $middleName, $lastName, $gender, $emailAddress, $contactTypeId, $contactData, $addressTypeId, $address, $city, $stateLocated);
+            echo 'After the files now';
+            exit;
 
             // Check and report the result
             if($regOutcome === 1){
