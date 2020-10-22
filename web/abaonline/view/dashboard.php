d400db4efce6ebbd8433774bbdfc64bbe4e94143
Sunday Onwuchekwa <donsonde@gmail.com>
2020-10-22 01:55:53 +0000

    Names merged

--- web/abaonline/view/dashboard.php
+++ web/abaonline/view/dashboard.php
@@ -59,22 +59,13 @@
           <div class="card-body">            
             <div class="row">
               <div class="col-sm-3">
-                <h6 class="mb-0">First Name</h6>
+                <h6 class="mb-0">Full Name</h6>
               </div>
               <div class="col-sm-9 text-secondary">
-                <?php echo $firstName; ?>
+                <?php echo $firstName . ' ' . $lastName; ?>
               </div>
             </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <h6 class="mb-0">Last Name</h6>
-              </div>
-              <div class="col-sm-9 text-secondary">
-                <?php echo $lastName; ?>
-              </div>
-            </div>
-            <hr>
+            <hr>            
             <div class="row">
               <div class="col-sm-3">
                 <h6 class="mb-0">Gender</h6>
