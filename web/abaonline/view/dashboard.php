0bff995d5e2302d21be51790d4f43b124f73ea3d
Sunday Onwuchekwa <donsonde@gmail.com>
2020-10-22 01:49:07 +0000

    text

--- web/abaonline/view/dashboard.php
+++ web/abaonline/view/dashboard.php
@@ -35,17 +35,24 @@
         <div class="card">
           <div class="card-body">
             <div class="d-flex flex-column align-items-center text-center">
-              <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+              <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle">
               <div class="mt-3">
                 <h4><?php echo $userName; ?></h4>
                 <p class="text-muted font-size-sm"><?php echo $city . ', '. $stateLocated; ?></p>
-                <button class="btn btn-primary">Edit</button>
-                <button class="btn btn-danger">Delete</button>
-                <button class="btn btn-outline-primary">Message</button>
+                <button class="btn btn-primary">Edit Profile</button>
+                <button class="btn btn-outline-primary">Send a Message</button>
               </div>
             </div>
           </div>
-        </div>        
+        </div> 
+        <div class="card mt-3">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+              <button class="btn btn-secondary mb-0">Add a Company</button>
+              <button class="btn btn-danger">Delete Account</button>
+            </li>
+          </ul>
+        </div>       
       </div>
       <div class="col-md-8">
         <div class="card mb-3">
@@ -101,6 +108,24 @@
               </div>
               <div class="col-sm-9 text-secondary">
                 <?php echo $address . ', ' . $city . ', ' . $stateLocated; ?>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row gutters-sm">
+          <div class="col-sm-12 mb-3">
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-sm-8">
+                    <h6 class="mb-0 text-secondary">Mattmeral Academy</h6>
+                  </div>
+                  <div class="col-sm-4">
+                    <button class="btn btn-primary">Edit</button>
+                    <button class="btn btn-secondary">Delete</button>
+                  </div>                  
+                </div>
               </div>
             </div>
           </div>
