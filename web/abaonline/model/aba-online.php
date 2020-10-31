6e912359f6c59b26343eadfe35c5f0dfb16872fe
Sunday Onwuchekwa <donsonde@gmail.com>
2020-10-31 20:33:10 +0000

    3

--- web/abaonline/model/aba-online.php
+++ web/abaonline/model/aba-online.php
@@ -356,8 +356,8 @@ function updateBusinessOwnerPassword($hashedPassword, $userName) {
   // The next four lines replace the placeholders in the SQL
   // statement with the actual values in the variables
   // and tells the database the type of data it is
-  $stmt->bindValue(':user_name', $clientId, PDO::PARAM_STR);
-  $stmt->bindValue(':password', $userName, PDO::PARAM_STR);
+  $stmt->bindValue(':user_name', $userName, PDO::PARAM_STR);
+  $stmt->bindValue(':password', $hashedPassword, PDO::PARAM_STR);
   // Insert the data
   $stmt->execute();
   // Ask how many rows changed as a result of our insert
