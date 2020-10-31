b6e835c28d3bad09fa987a1adb051f90227f1873
Sunday Onwuchekwa <donsonde@gmail.com>
2020-10-31 21:57:31 +0000

    n

--- web/abaonline/view/business-owner-update.php
+++ web/abaonline/view/business-owner-update.php
@@ -138,7 +138,7 @@
                                     <div class="form-group row">
                                         <label for="addressTypeId" class="col-sm-4 col-form-label">Address Type</label>
                                         <div class="col-sm-8">
-                                            <?php echo $bindAddressList; ?>
+                                            <?php echo $addressTypesList; ?>
                                         </div>
                                     </div>
                                     <div class="form-group row">
