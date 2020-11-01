08e8ae662fe2d807b1a3d9117218c1c5840c1c7a
Sunday Onwuchekwa <donsonde@gmail.com>
2020-11-01 05:27:32 +0000

    b

--- web/abaonline/library/functions.php
+++ web/abaonline/library/functions.php
@@ -77,10 +77,10 @@ function buildCompanyList($companies) {
 
 function buildCompanyDisplay($companyInfos) {
     $pd = '<h1 class="text-center font-weight-bold">FEATURED BUSINESS LISTINGS</h1>';
-    $pd .= '<div class="row mb-3">';
+    $pd .= '<div class="row card-deck">';
     foreach ($companyInfos as $companyData) {        
         $pd .= '<div class="col-sm-12 col-md-6 col-lg-4">';
-        $pd .= '<div class="card h-100  mb-3">';
+        $pd .= '<div class="card mb-3">';
         $pd .= '<div class="card-body">';
         $pd .= "<a class='container' href='/abaonline/actions/index.php?action=view_company&company_id=$companyData[company_id]' title='Click to view this company'>";
         $pd .= "<img src='https://bootdey.com/img/Content/avatar/avatar7.png' alt='Admin' class='rounded-circle'>";
